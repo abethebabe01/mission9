dbe8ed5cf1509a17a895c14e214a68b538418233
abethebabe01 <103139893+abethebabe01@users.noreply.github.com>
2024-03-04 09:39:10 -0700

    Updating my assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import teamsData from './CollegeBasketballTeams.json';
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+// insert team names in here.
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>march madness time</h1>
+      <h3>come meet the teams! go cougs!</h3>
+    </header>
+  );
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props; //teamsData.teams.find((team) => team.tid === 3);
+
+    // Check if oneTeam is not null before accessing its properties
+    if (!oneTeam) {
+      return (
+        <div>
+          <h2>No team found.</h2>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <h2>School: {oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamsList() {
+  return (
+    <div className="team-container">
+      {teamsData.teams.map((team: TeamProps, index: number) => (
+        <Team
+          key={index}
+          school={team.school}
+          name={team.name}
+          city={team.city}
+          state={team.state}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Fortnite() {
+  return <h1>Fortnite Battlepass.</h1>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <TeamsList />
     </div>
   );
 }
